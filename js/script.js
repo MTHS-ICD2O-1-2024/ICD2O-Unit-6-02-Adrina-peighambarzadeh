9a9286f9cea80b64f9bc85a07708399561224820
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-14 17:33:08 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,48 +1,56 @@
- // Copyright (c) 2025 Ain Jeong All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Adrina peighambarzadeh
+// Created by: Adrina Peighambarzadeh
 // Created on: May 2025
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict";
 
 /**
- * Check servie worker.
+ * This function calculates and updates the number of cookies
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICD2O-Unit-6-01-Adrina-peighambarzadeh/sw.js", {
-    scope: "/ICD2O-Unit-6-01-Adrina-peighambarzadeh/",
-  })
-}
+function cookieClicker() {
+  // Retrieve the current cookie count from localStorage
+  let cookieClick = localStorage.getItem("cookie-click");
 
-/**
- * This function calculate, doing division
- */
-// eslint-disable-next-line no-unused-vars
-function doDivision () {
-  let counter = 0
-
-  // input
-  let firstNumber = parseFloat(
-    document.getElementById('first-number').value
-  )
-  const secondNumber = parseFloat(
-    document.getElementById('second-number').value
-  )
-
-  // process
-  while (firstNumber >= secondNumber) {
-    firstNumber -= secondNumber
-    counter++
-
-    if (firstNumber < secondNumber) {
-      break
-    }
+  // If there is a stored cookie count, increment it by 1
+  if (cookieClick) {
+    cookieClick = Number(cookieClick) + 1;
+  } else {
+    // Otherwise, initialize cookie count to 1
+    cookieClick = 1;
   }
 
-  // output
-  document.getElementById('answer').innerHTML =
-    'Your answer is: ' +
-    counter +
-    ' R ' + firstNumber
+  // Save the updated cookie count back to localStorage
+  localStorage.setItem("cookie-click", cookieClick.toString());
+
+  // Update the display with the new cookie count
+  checkCookies(cookieClick);
 }
+
+/**
+ * This function updates the displayed number of cookies
+ * @param {number} cookieCount - The current count of cookies
+ */
+function checkCookies(cookieCount) {
+  // Check if the element with id "answer" exists
+  const answerElement = document.getElementById("answer");
+
+  if (answerElement) {
+    if (cookieCount === 1) {
+      answerElement.innerHTML = cookieCount + " cookie"; // Singular form
+    } else {
+      answerElement.innerHTML = cookieCount + " cookies"; // Plural form
+    }
+  }
+}
+
+// Check and display the initial number of cookies when the page loads
+window.onload = function () {
+  const cookieClick = localStorage.getItem("cookie-click");
+  if (cookieClick) {
+    checkCookies(Number(cookieClick)); // Display the saved cookie count
+  } else {
+    checkCookies(0); // If no cookies in localStorage, start from 0
+  }
+};
